bbbbe55fe12ea0ed274270ac816d21b626288565
juanmabaal <juanma.baal@gmail.com>
2023-07-30 18:27:34 -0500

    Se le dio estilo a las ordenes de compra

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,4 +1,4 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 import { useContext } from "react"
 import { ShoppingCartContext } from "../../Context";
 
@@ -7,11 +7,18 @@ const OrdersCard = props => {
     const {totalPrice, totalProducts} = props
 
     return (
-        <div className='flex justify-between items-center mb-3'>
-            <p>
-                <span>27.07.23</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
+        <div className='flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80 '>
+            <p className='flex justify-between w-full text-black'>
+                <div className='flex flex-col'>
+                    <span className='font-light'>27.07.23</span>
+                    <span className='font-light'>{totalProducts} articles</span>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <span   className='font-medium text-2xl text-red-700'>${totalPrice}</span>
+                    <ChevronRightIcon 
+                    className='h-6 w-6 text-black'>
+                    </ChevronRightIcon>
+                </div>
             </p>
         </div>
     )
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -10,7 +10,7 @@ function MyOrders() {
     return (
         <Layout>
           <div className='flex items-center justify-center relative w-80'>
-            <h1>My Orders</h1>
+            <h1 class="font-medium text-xl mb-4">My Orders</h1>
           </div>
         {
         context.order.map(( order, index) => (

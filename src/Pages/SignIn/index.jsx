7e535ba34e3fe1067c6772f32f3132a72c80df3b
juanmabaal <juanma.baal@gmail.com>
2023-08-18 18:57:04 -0500

    Se agrego la funcionalidad para saber para saber si el usuario tiene cuenta o no para mostrar los datos y habilitar/deshabilitar botones

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,9 +1,21 @@
 import Layout from "../../Components/Layout"
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
 import { Link } from 'react-router-dom'
 import React, {useState } from "react"
 
 
 function SignIn() {
+  const context = useContext(ShoppingCartContext)
+
+  //Account
+  const account = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(account)
+
+  //Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 
     return (
         <Layout>
@@ -11,16 +23,17 @@ function SignIn() {
           <div className='flex flex-col w-80'>
             <p>
               <span className='font-light text-sm'>Email: </span>
-              <span>juanma.baal@gmail.com</span>
+              <span>{parsedAccount?.email}</span>
             </p>
             <p>
               <span className='font-light text-sm'>Password: </span>
-              <span>********</span>
+              <span>{parsedAccount?.password}</span>
             </p>
             <Link
               to="/">
               <button
-                className='bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2'>
+                className='bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2'
+                disabled={!hasUserAnAccount}>
                 Log in
               </button>
             </Link>
@@ -28,7 +41,8 @@ function SignIn() {
               <a className='font-light text-xs underline underline-offset-4' href='/'>Forgot my password</a>
             </div>
             <button
-              className='border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3'>
+              className='border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3'
+              disabled={hasUserAnAccount}>
               Sign up
             </button>
           </div>
